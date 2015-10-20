f2ae0125a798d4fd634c9d29b5da8c4bc164007b
fatbeans <xindianden@gmail.com>
2015-10-20 16:36:53 +0800

    新增工单派发接口

--- JFinal/src/controller/ConclusionStatController.java
+++ JFinal/src/controller/ConclusionStatController.java
@@ -8,9 +8,11 @@ import com.jfinal.plugin.activerecord.DbKit;
 import dic.DbType;
 import export.ExcelExport;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
 import org.apache.commons.lang.time.DateFormatUtils;
 
 import java.io.File;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,39 +23,112 @@ import java.util.*;
  */
 public class ConclusionStatController extends Controller {
 
+
+    private String mdn2Imsi(String mdn) throws SQLException {
+//
+        Connection connection = DbKit.getConfig(DbType.ORACLE.getValue()).getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select IMSI from map_imsi_mdn where " +
+                "mdn=" + mdn);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            String imsi = resultSet.getString(1);
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+            return imsi;
+        } else {
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+            return null;
+        }
+    }
+
+    /**
+     * 地市下拉数据
+     *
+     * @throws SQLException
+     */
+    public void getCity() throws SQLException {
+        String s = PropKit.get("CITY_SQL");
+        Connection connection = DbKit.getConfig(DbType.ORACLE.getValue()).getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(s);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        JSONArray array = new JSONArray();
+        while (resultSet.next()) {
+            if (resultSet.getString(1) != null && resultSet.getString(2) != null) {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("id", resultSet.getString(1));
+                jsonObject.put("name", resultSet.getString(2));
+                array.add(jsonObject);
+            }
+        }
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        renderJson(array.toJSONString());
+    }
+
+    /**
+     * 结论下拉数据
+     */
+    public void getConCfg() throws SQLException {
+        String s = PropKit.get("CON_CFG_SQL");
+        Connection connection = DbKit.getConfig(DbType.ORACLE.getValue()).getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(s);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        JSONArray array = new JSONArray();
+        while (resultSet.next()) {
+            if (resultSet.getString(1) != null && resultSet.getString(2) != null && resultSet.getString(3) != null &&
+                    resultSet.getString(4) != null) {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("lv1id", resultSet.getString(1));
+                jsonObject.put("lv1name", resultSet.getString(2));
+                jsonObject.put("lv2id", resultSet.getString(3));
+                jsonObject.put("lv2name", resultSet.getString(4));
+                array.add(jsonObject);
+            }
+        }
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        renderJson(array.toJSONString());
+    }
+
+
+    /**
+     * 查询结论
+     *
+     * @throws SQLException
+     */
     public void concUnder() throws SQLException {
         String sd = getPara("sd", DateFormatUtils.format(new Date(), "yyyyMMdd"));
+        String ed = getPara("ed", DateFormatUtils.format(new Date(), "yyyyMMdd"));
+        long msisdn = getParaToLong("msisdn", 0l);
+        if ((msisdn + "").length() == 11) {
+            String imsi = mdn2Imsi(msisdn + "");
+            if (imsi == null) {
+                renderError(488);
+            } else {
+                msisdn = NumberUtils.toLong(imsi, 0l);
+            }
+        }
+
         sd = StringUtils.rightPad(sd, 6, "0").substring(0, 6);
         long lv2_con_id = getParaToLong("lv2_con_id", -1l);
+        int cityId = getParaToInt("cityId", 0);
 
         List<Map<String, String>> data = new ArrayList<Map<String, String>>();
-//        DNS服务器出错 DNS域名服务器不支持请求的类型 DNS拒绝服务
-        if (lv2_con_id == 10020005 || lv2_con_id == 10020006 || lv2_con_id == 10020007) {
-            getDnsEcStatData(lv2_con_id, data);
-        }
-//        用户侧DNS解析时延问题 核心侧DNS解析时延问题
-        else if (lv2_con_id == 10030008 || lv2_con_id == 10020008) {
-            getDnsDelayData(data);
-        }
-//        核心侧·质量问题 核心侧·高负荷 核心侧·核查参数
-        else if (lv2_con_id == 10020002 || lv2_con_id == 10020001 || lv2_con_id == 10020009) {
-            getCtlMmeData(sd, lv2_con_id, data);
-        }
-//        弱覆盖 质差小区 高负荷
-        else if (lv2_con_id == 10010001 || lv2_con_id == 10010002 || lv2_con_id == 10010003) {
-            getCtlCellData(sd, lv2_con_id, data);
-        }
-//        业务属网外资源
-        else if(lv2_con_id == 10050001){
-            getHttpSpResData(sd, lv2_con_id, data);
-        }
+
+        getCtlCellData(sd, ed, lv2_con_id, data, cityId, msisdn);
+
 
         JSONObject json = new JSONObject();
         json.put("rows", data);
         System.out.println(json.toJSONString());
         renderJson(json.toJSONString());
-
-
     }
 
     private void getHttpSpResData(String sd, long lv2_con_id, List<Map<String, String>> data) throws SQLException {
@@ -68,7 +143,7 @@ public class ConclusionStatController extends Controller {
             for (int i = 0; i < resultSet.getMetaData().getColumnCount(); i++) {
                 try {
                     row.put(resultSet.getMetaData().getColumnName(i + 1), resultSet.getObject(i + 1).toString());
-                }catch (NullPointerException e){
+                } catch (NullPointerException e) {
 
                 }
             }
@@ -79,21 +154,31 @@ public class ConclusionStatController extends Controller {
         preparedStatement.close();
     }
 
-    private void getCtlCellData(String sd, long lv2_con_id, List<Map<String, String>> data) throws SQLException {
-        String sql = PropKit.get("CTL_CELL_STAT");
+    private void getCtlCellData(String sd, String ed, long lv2_con_id, List<Map<String, String>> data, int cityId,
+                                long msisdn)
+            throws
+            SQLException {
+        String sql = PropKit.get("VIEW_LIST_STAT");
+        if (msisdn != 0) {
+            sql = sql.replace("1=1", "a.usr=" + msisdn);
+        }
         PreparedStatement preparedStatement = DbKit.getConfig(DbType.ORACLE.getValue()).getConnection()
                 .prepareStatement(sql);
+        int startTime = NumberUtils.toInt(sd);
+        int endTime = NumberUtils.toInt(ed);
         preparedStatement.setLong(1, lv2_con_id);
-        preparedStatement.setString(2, sd);
+        preparedStatement.setInt(2, cityId);
+        preparedStatement.setInt(3, startTime);
+        preparedStatement.setInt(4, endTime);
         ResultSet resultSet = preparedStatement.executeQuery();
         while (resultSet.next()) {
             HashMap<String, String> row = new HashMap<String, String>();
             for (int i = 0; i < resultSet.getMetaData().getColumnCount(); i++) {
-                    try {
-                        row.put(resultSet.getMetaData().getColumnName(i + 1), resultSet.getObject(i + 1).toString());
-                    }catch (NullPointerException e){
+                try {
+                    row.put(resultSet.getMetaData().getColumnName(i + 1), resultSet.getObject(i + 1).toString());
+                } catch (NullPointerException e) {
 
-                    }
+                }
             }
             data.add(row);
         }
@@ -154,7 +239,7 @@ public class ConclusionStatController extends Controller {
             for (int i = 0; i < resultSet.getMetaData().getColumnCount(); i++) {
                 try {
                     row.put(resultSet.getMetaData().getColumnName(i + 1), resultSet.getObject(i + 1).toString());
-                }catch (NullPointerException e){
+                } catch (NullPointerException e) {
 
                 }
             }
@@ -169,21 +254,19 @@ public class ConclusionStatController extends Controller {
         String jsStr = getPara("data");
         if (jsStr != null) {
             JSONArray array = JSONArray.parseArray(jsStr);
-            array.getJSONObject(1);
 
-
-            String[] header = new String[]{"网元类型", "网元名称", "网元归属地市", "诊断问题", "影响人次"};
-            String[] key = new String[]{"NETYPE","NENAME","NECITY","LV2_CON_NAME","SCNT"};
+            String[] header = new String[]{"网元类型", "网元名称", "网元归属地市", "诊断问题", "影响人次" };
+            String[] key = new String[]{"NETYPE", "NENAME", "NECITY", "LV2_CON_NAME", "SCNT" };
 
             String fileName = PropKit.get("FILE_PRE") + DateFormatUtils.format(new Date(),
-                    "yyyyMmddHHmmssSSS")+".xlsx";
+                    "yyyyMmddHHmmssSSS") + ".xlsx";
             System.out.println(fileName);
             File file = new File(PropKit.get("FILE_PATH") + fileName);
 
-            ExcelExport.createExcelFile(file,array,header,key);
+            ExcelExport.createExcelFile(file, array, header, key);
             renderText(fileName);
 
-        }else{
+        } else {
             renderError(501);
         }
     }
